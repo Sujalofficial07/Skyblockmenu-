205af3774da28742754debac1bf16db0a9a2b922
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-03-30 18:01:28 +0530

    Update HypixelCore.java

--- src/main/java/com/sujal/hypixelcore/HypixelCore.java
+++ src/main/java/com/sujal/hypixelcore/HypixelCore.java
@@ -1,24 +1,32 @@
 package com.sujal.hypixelcore;
 
 import com.sujal.hypixelcore.commands.MenuCommand;
+import com.sujal.hypixelcore.data.PlayerManager;
 import com.sujal.hypixelcore.listeners.MenuListener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class HypixelCore extends JavaPlugin {
+public final class HypixelCore extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
         getLogger().info("HypixelCore plugin has been enabled!");
         
-        // Registering Commands & Listeners
         getCommand("menu").setExecutor(new MenuCommand());
         getServer().getPluginManager().registerEvents(new MenuListener(), this);
+        getServer().getPluginManager().registerEvents(this, this); // Register Join/Quit
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-        getLogger().info("HypixelCore plugin has been disabled!");
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        PlayerManager.loadPlayer(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        PlayerManager.unloadPlayer(event.getPlayer());
     }
 }
