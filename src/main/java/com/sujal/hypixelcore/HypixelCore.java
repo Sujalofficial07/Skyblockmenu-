ebfa3bb65ef5f489a817fb17f0994e1cea2705e2
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-03-30 18:28:51 +0530

    Update HypixelCore.java

--- src/main/java/com/sujal/hypixelcore/HypixelCore.java
+++ src/main/java/com/sujal/hypixelcore/HypixelCore.java
@@ -3,26 +3,44 @@ package com.sujal.hypixelcore;
 import com.sujal.hypixelcore.commands.MenuCommand;
 import com.sujal.hypixelcore.data.PlayerManager;
 import com.sujal.hypixelcore.listeners.MenuListener;
+import com.sujal.hypixelcore.menu.SkyblockMenu;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HypixelCore extends JavaPlugin implements Listener {
 
+    private static HypixelCore instance;
+
     @Override
     public void onEnable() {
-        getLogger().info("HypixelCore plugin has been enabled!");
+        instance = this;
+        getLogger().info("HypixelCore (Ultimate Edition) Enabled!");
         
+        // Register Commands & Listeners
         getCommand("menu").setExecutor(new MenuCommand());
         getServer().getPluginManager().registerEvents(new MenuListener(), this);
-        getServer().getPluginManager().registerEvents(this, this); // Register Join/Quit
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    public static HypixelCore getInstance() {
+        return instance;
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         PlayerManager.loadPlayer(event.getPlayer());
+        // Automatically give the unbreakable menu star on join
+        SkyblockMenu.giveMenuStar(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+        // Ensure they get the star back if they die
+        SkyblockMenu.giveMenuStar(event.getPlayer());
     }
 
     @EventHandler
