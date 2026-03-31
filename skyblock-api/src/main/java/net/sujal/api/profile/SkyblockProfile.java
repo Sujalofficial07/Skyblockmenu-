565abf0176c942db2b2fde24f0f38cf2b249c933
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-03-31 12:34:21 +0530

    Update SkyblockProfile.java

--- skyblock-api/src/main/java/net/sujal/api/profile/SkyblockProfile.java
+++ skyblock-api/src/main/java/net/sujal/api/profile/SkyblockProfile.java
@@ -1,6 +1,9 @@
 // Path: /skyblock-api/src/main/java/net/sujal/api/profile/SkyblockProfile.java
 package net.sujal.api.profile;
 
+import net.sujal.api.rpg.Skill;
+import net.sujal.api.rpg.Stat;
+
 import java.util.UUID;
 
 /**
@@ -9,10 +12,18 @@ import java.util.UUID;
 public interface SkyblockProfile {
     UUID getPlayerUuid();
     
+    // Economy
     double getCoins();
     void setCoins(double amount);
     void addCoins(double amount);
     void removeCoins(double amount);
     
-    // Future expansion: Stats, Skills, Collections will be accessible here.
+    // Skills
+    double getSkillXp(Skill skill);
+    void addSkillXp(Skill skill, double amount);
+    void setSkillXp(Skill skill, double amount);
+
+    // Stats
+    double getBaseStat(Stat stat);
+    void setBaseStat(Stat stat, double amount);
 }
