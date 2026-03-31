5c1284b025a3d103b7f9784f05ecf53a0d3da5d7
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-03-31 08:20:39 +0530

    Create StatSource.java

---
+++ src/main/java/sb/sujal/stats/StatSource.java
@@ -0,0 +1,11 @@
+package sb.sujal.stats;
+
+public enum StatSource {
+    BASE,       // Natural level up / Fairy Souls
+    SKILL,      // Farming, Combat, etc.
+    ARMOR,      // Helmet, Chestplate, etc.
+    WEAPON,     // Held item
+    ACCESSORY,  // Talismans
+    PET,        // Active Pet
+    TEMPORARY   // Potions / Buffs
+}
