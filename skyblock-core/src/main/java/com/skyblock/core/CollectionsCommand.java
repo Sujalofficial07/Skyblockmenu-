ab4a4f66951734557720d58eb137b33e3575ccb6
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-03-31 16:08:22 +0530

    Create CollectionsCommand.java

---
+++ skyblock-core/src/main/java/com/skyblock/core/CollectionsCommand.java
@@ -0,0 +1,33 @@
+package com.skyblock.core;
+
+import com.skyblock.collections.CollectionsMenu;
+import com.skyblock.menu.MenuManager;
+import com.skyblock.utils.ColorUtil;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+public class CollectionsCommand implements CommandExecutor {
+
+    private final Plugin plugin;
+    private final MenuManager menuManager;
+
+    public CollectionsCommand(Plugin plugin, MenuManager menuManager) {
+        this.plugin = plugin;
+        this.menuManager = menuManager;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command,
+                             @NotNull String label, @NotNull String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage(ColorUtil.color("&cOnly players can use this command."));
+            return true;
+        }
+        menuManager.openMenu(player, new CollectionsMenu(menuManager));
+        return true;
+    }
+}
