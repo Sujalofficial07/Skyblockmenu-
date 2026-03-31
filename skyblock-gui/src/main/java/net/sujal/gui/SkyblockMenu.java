4d3024a9f3a488625327fb565a4ff8ad580400cb
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-03-31 14:50:25 +0530

    Delete skyblock-gui directory

--- skyblock-gui/src/main/java/net/sujal/gui/SkyblockMenu.java
+++
@@ -1,9 +0,0 @@
-// Path: /skyblock-gui/src/main/java/net/sujal/gui/SkyblockMenu.java
-package net.sujal.gui;
-
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.InventoryHolder;
-
-public interface SkyblockMenu extends InventoryHolder {
-    void handleClick(InventoryClickEvent event);
-}
