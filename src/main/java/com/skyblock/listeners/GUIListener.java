d9a50c7b628c53efdcb18ad7f45b9da765d0b4df
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-04-01 15:45:18 +0530

    Update GUIListener.java

--- src/main/java/com/skyblock/listeners/GUIListener.java
+++ src/main/java/com/skyblock/listeners/GUIListener.java
@@ -5,18 +5,22 @@ import com.skyblock.utils.NBTUtil;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.*;
 
 public class GUIListener implements Listener {
 
     @EventHandler
     public void onClick(InventoryClickEvent e) {
+
+        if (e.getView().getTitle().contains("SkyBlock") ||
+                e.getView().getTitle().contains("Stats")) {
+            e.setCancelled(true);
+        }
+
         if (e.getCurrentItem() == null) return;
 
-        String menu = NBTUtil.getString(e.getCurrentItem(),"menu");
-        if(menu == null) return;
-
-        e.setCancelled(true);
+        String menu = NBTUtil.getString(e.getCurrentItem(), "menu");
+        if (menu == null) return;
 
         Player p = (Player) e.getWhoClicked();
 
@@ -25,6 +29,13 @@ public class GUIListener implements Listener {
             case "collections" -> CollectionsMenu.open(p);
             case "stats" -> StatsMenu.open(p);
             case "profile" -> MainMenu.open(p);
+        }
+    }
+
+    @EventHandler
+    public void onDrag(InventoryDragEvent e) {
+        if (e.getView().getTitle().contains("SkyBlock")) {
+            e.setCancelled(true);
         }
     }
 }
