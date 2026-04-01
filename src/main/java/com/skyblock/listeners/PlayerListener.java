a0b4da5e640543d5e29d938f331be7e0bea464b5
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-04-01 08:56:43 +0530

    Create PlayerListener.java

---
+++ src/main/java/com/skyblock/listeners/PlayerListener.java
@@ -0,0 +1,13 @@
+package com.skyblock.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class PlayerListener implements Listener {
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent e) {
+        // Reserved for future expansion
+    }
+}
