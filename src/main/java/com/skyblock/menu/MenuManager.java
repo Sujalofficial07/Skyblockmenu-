18f0e50ca9f086dae971498dd38b33386b958637
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-04-01 15:43:03 +0530

    Update MenuManager.java

--- src/main/java/com/skyblock/menu/MenuManager.java
+++ src/main/java/com/skyblock/menu/MenuManager.java
@@ -1,31 +1,45 @@
 package com.skyblock.menu;
 
+import com.skyblock.utils.ItemBuilder;
+import com.skyblock.utils.NBTUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 
-import com.skyblock.utils.NBTUtil;
+public class MainMenu {
 
-public class MenuManager {
-
-    public static void openMainMenu(Player player) {
+    public static void open(Player player) {
         Inventory inv = Bukkit.createInventory(null, 54, "SkyBlock Menu");
 
-        ItemStack skills = new ItemStack(Material.DIAMOND_SWORD);
-        NBTUtil.setString(skills, "menu", "skills");
+        for (int i = 0; i < 54; i++) {
+            inv.setItem(i, new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE)
+                    .name("§7")
+                    .build());
+        }
 
-        ItemStack collections = new ItemStack(Material.CHEST);
-        NBTUtil.setString(collections, "menu", "collections");
+        inv.setItem(13, createItem(Material.PLAYER_HEAD, "profile"));
+        inv.setItem(20, createItem(Material.DIAMOND_SWORD, "skills"));
+        inv.setItem(21, new ItemBuilder(Material.PAPER).name("§aCollections").build());
+        inv.setItem(22, new ItemBuilder(Material.BOOK).name("§aRecipe Book").build());
+        inv.setItem(23, new ItemBuilder(Material.GRASS_BLOCK).name("§aSkyBlock Levels").build());
+        inv.setItem(24, createItem(Material.CLOCK, "stats"));
+        inv.setItem(25, new ItemBuilder(Material.CHEST).name("§aEnder Chest").build());
 
-        ItemStack stats = new ItemStack(Material.NETHER_STAR);
-        NBTUtil.setString(stats, "menu", "stats");
+        inv.setItem(29, new ItemBuilder(Material.CAKE).name("§aProfile").build());
+        inv.setItem(30, new ItemBuilder(Material.BONE).name("§aPets").build());
+        inv.setItem(31, new ItemBuilder(Material.CRAFTING_TABLE).name("§aCrafting").build());
+        inv.setItem(32, new ItemBuilder(Material.LEATHER_CHESTPLATE).name("§aWardrobe").build());
+        inv.setItem(33, new ItemBuilder(Material.PLAYER_HEAD).name("§aEquipment").build());
 
-        inv.setItem(20, skills);
-        inv.setItem(22, collections);
-        inv.setItem(24, stats);
+        inv.setItem(48, new ItemBuilder(Material.PLAYER_HEAD).name("§aClose").build());
 
         player.openInventory(inv);
+    }
+
+    private static org.bukkit.inventory.ItemStack createItem(Material mat, String id) {
+        org.bukkit.inventory.ItemStack item = new ItemBuilder(mat).name("§a" + id).build();
+        NBTUtil.setString(item, "menu", id);
+        return item;
     }
 }
