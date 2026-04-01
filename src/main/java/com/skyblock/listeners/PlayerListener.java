7210077a96a9b6a4831a4cba446c89ddf46ddb2e
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-04-01 10:06:06 +0530

    Update PlayerListener.java

--- src/main/java/com/skyblock/listeners/PlayerListener.java
+++ src/main/java/com/skyblock/listeners/PlayerListener.java
@@ -1,13 +1,14 @@
 package com.skyblock.listeners;
 
+import com.skyblock.stats.StatsManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerListener implements Listener {
 
     @EventHandler
-    public void onInteract(PlayerInteractEvent e) {
-        // Reserved for future expansion
+    public void onJoin(PlayerJoinEvent e){
+        StatsManager.init(e.getPlayer().getUniqueId());
     }
 }
